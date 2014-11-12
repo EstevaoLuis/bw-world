0f3ab8cd4f4e806a53a600e6a44301579ee74141
Mastercava <mastercava@hotmail.it>
2014-11-12 08:24:52 +0100

    First commit

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var directionVector = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+	}
+}
