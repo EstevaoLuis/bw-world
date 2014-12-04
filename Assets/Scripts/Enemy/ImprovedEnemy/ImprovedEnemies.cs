703ed29dba4a30d230f99b0ce25f91c52e7c3a82
Mastercava <mastercava@hotmail.it>
2014-12-04 12:20:31 +0100

    Bugs

--- Assets/Scripts/Enemy/ImprovedEnemy/ImprovedEnemies.cs
+++ Assets/Scripts/Enemy/ImprovedEnemy/ImprovedEnemies.cs
@@ -16,7 +16,12 @@ public class ImprovedEnemies : MonoBehaviour {
 	public float speed = 100f;
 	public GameObject target;
 
+	//Enemy parameters
 	public int life;
+
+	private float deadTime;
+	private bool isAlive = true;
+
 	public GameObject spell_1;
 	public GameObject spell_2;
 	public GameObject melee;
@@ -67,10 +72,10 @@ public class ImprovedEnemies : MonoBehaviour {
 
 		if (choose_attack () == 1) {
 		//spell 1
-			generate_spell_1();
+			GameInstance.instance.castSpell("Red 1",transform,direction,"SpellEnemy");
 		} else if (choose_attack () == 0) {
 		//spell 2
-			generate_spell_2();
+			GameInstance.instance.castSpell("Black 1",transform,direction,"SpellEnemy");
 		} else if (choose_attack () == -1) {
 		//melee
 			generate_melee();
@@ -87,7 +92,7 @@ public class ImprovedEnemies : MonoBehaviour {
 
 	}
 
-
+	/*
 	void generate_spell_1(){
 
 		GameObject attack;
@@ -101,6 +106,8 @@ public class ImprovedEnemies : MonoBehaviour {
 		attack=(GameObject)Instantiate(spell_2,new Vector3(transform.position.x+direction.x,transform.position.y+direction.y,0),transform.rotation);
 		
 	}
+	*/
+
 	void generate_melee(){
 		
 		GameObject attack;
@@ -181,18 +188,18 @@ public class ImprovedEnemies : MonoBehaviour {
 		rigidbody2D.velocity = Vector3.left * speed;
 	}
 
-//	void Collision (Collider col){
-//		if (col.ta == true) {
-//			life--;		
-//		}
-//
-//	}
+
 	void OnCollisionEnter2D(Collision2D other){
 		if (other.gameObject.tag == "Spell") {
-			life--;
-			if(life == 0){
-				Destroy(gameObject);
-			}
+				Spell spellParameters = (Spell)other.gameObject.GetComponent ("Spell");
+				life -= spellParameters.damage;
+				if (life <= 0) {
+						isAlive = false;
+						deadTime = Time.time;
+						//Destroy(gameObject);
+				}
+		} else if (other.gameObject.tag == "SpellEnemy") {
+			Destroy (other.gameObject);
 		}
 	}
 //	void collision_detected(){
@@ -216,32 +223,37 @@ public class ImprovedEnemies : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (isAlive) {
+				if (detect_player () != true) {
+						if (counter == max_move) {
+								rand = random_number ();
+								counter = 0;
+						}
+						movement_random (rand);
+						//print (counter);
+						counter++;
 
-		if (detect_player () != true) {
-			if (counter == max_move) {
-				rand = random_number ();
-				counter = 0;
+				} else {
+
+						move_towards_player ();
+
+				}
+				//collision_detected ();
+				//		Collision (this.collider);
+				//		Death_of_Enemy ();
+				if (counter_2 == max_fire_rate) {
+						//attacking ();
+
+						attacking ();
+
+						counter_2 = 0;
+				} else {
+						counter_2++;
+				}
+		} else {
+			if(Time.time>deadTime+0.2) {
+				Destroy (gameObject);
 			}
-			movement_random (rand);
-			//print (counter);
-			counter++;
-
-		} else {
-
-			move_towards_player ();
-
-		}
-		//collision_detected ();
-//		Collision (this.collider);
-//		Death_of_Enemy ();
-		if (counter_2 == max_fire_rate) {
-			//attacking ();
-		
-			attacking ();
-
-			counter_2=0;
-		} else {
-			counter_2++;
 		}
 	}
 }
