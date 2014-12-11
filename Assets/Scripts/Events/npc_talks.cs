e82f23cb7656efbc2d88f1be46eeb54f988fd18d
fixgamedev <felixlucashazen@gmail.com>
2014-12-11 13:15:53 +0100

    NPC Talks to you when you approach.

--- Assets/Scripts/Events/npc_talks.cs
+++ Assets/Scripts/Events/npc_talks.cs
@@ -6,12 +6,27 @@ public class npc_talks : MonoBehaviour {
 	// Use this for initialization
 	private GameObject player;
 	public string text_to_say;
+	private GUIText txt;
+	float dist;
 	float distance_to_talk = 2f;
 
 	void Start () {
 	
 		player = GameObject.FindGameObjectWithTag("Player");
-		//Instantiate(Txt,transform.position,transform.rotation);
+	  
+
+	}
+	void OnGUI() {
+
+		dist = Vector3.Distance (player.transform.position, this.transform.position);
+
+		if (dist < distance_to_talk) {
+
+			GUI.Label (new Rect (this.transform.position.x, this.transform.position.y, 100, 100), text_to_say);
+
+		}
+
+		GUI.TextField(new Rect (this.transform.position.x, this.transform.position.y, 100, 100), "");	
 
 	}
 	
@@ -22,22 +37,15 @@ public class npc_talks : MonoBehaviour {
 //				
 //		Txt = GameObject.FindGameObjectWithTag("Text");
 
-		float dist = Vector3.Distance (player.transform.position, this.transform.position);
-
 		print (dist);
 
-		if (dist < distance_to_talk) {
-
-			this.guiText.enabled = true;
-			this.guiText.text = text_to_say;
-
-				} else {
-			this.guiText.text = "";
-			this.guiText.enabled = false;
-			//this.guiText.text = "****";
-		}
-
-
+//			this.guiText.enabled = true;
+//			this.guiText.text = text_to_say;
+//
+//				} else {
+//			this.guiText.text = "";
+//			this.guiText.enabled = false;
+//		
 	}
 }
 
