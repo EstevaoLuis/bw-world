53d338c6fe3e2d616df48830ef19ee4f8fb7efde
fixgamedev <felixlucashazen@gmail.com>
2014-12-13 16:41:06 +0100

    Explosive charges 1.1
    Explosive prefab :)

--- Assets/Scripts/Mine.cs
+++ Assets/Scripts/Mine.cs
@@ -4,19 +4,50 @@ using System.Collections;
 public class Mine : MonoBehaviour {
 
 	public GameObject exp;
+	//private Animator exp_anim;
+	private GameObject exp_cop;
+	int lifes = 3;
+
+	//private GameObject exp_cop;
 	// Use this for initialization
 	void Start () {
 	
+		//exp_anim = exp.GetComponent<Animator> ();
+		//exp = GameObject.FindGameObjectWithTag("mine");
+
 	}
 
 	void OnCollisionEnter2D (Collision2D other){
-		
-		Instantiate (exp, transform.position,transform.rotation);
-		//Destroy (gameObject);
+
+		lifes--;
+		exp_cop = (GameObject)Instantiate (exp, transform.position,transform.rotation);
+		//exp.animation.wrapMode = WrapMode.Once;
+
+
+		if (lifes == 1) {
+			gameObject.renderer.material.color = Color.red;
+		}
+
+//		
+		if (lifes == 0) {
+
+
+			Destroy (other.gameObject);
+			Destroy(gameObject);
+
+		}
+//		if (counter == max_counter) {
+//			Destroy (exp_cop);
+//			counter = 0;
+//		}
+//		counter ++;
+//	
 	}
 
 	// Update is called once per frame
 	void Update () {
-	
+
+		//exp.animation.wrapMode = WrapMode.Once;
+
 	}
 }
