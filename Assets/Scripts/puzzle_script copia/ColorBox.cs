75a48b4d6c82b4fab6299d79f941f27dc5eab5e0
Estevao Luis <estevao.costamoura@dev4side.com>
2014-12-17 14:48:23 +0100

    Puzzle System
    scene puzzle

---
+++ Assets/Scripts/puzzle_script copia/ColorBox.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorBox : MonoBehaviour {
+
+	// 0 = Red
+	// 1 = Green
+	// 2 = Blue
+	public int color = 0;
+	//public GameObject puzzleMaster = null;
+	//private PuzzleMaster pm;
+	public PuzzleMaster pm = null;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		if (pm.active == true && other.gameObject.tag == "Player") {
+			if (color != pm.correctSequence[pm.attempt]){
+				Debug.Log("WRONG! " + pm.attempt);
+				pm.attempt = 0;
+				return;
+			}
+			var a = ++pm.attempt;
+			Debug.Log("CORRECT! " + pm.attempt);
+			//Debug.Log("HEYSUKE! " + pm.Size ());
+			if (pm.Size() == a){
+				Debug.Log("COMPLETE! " + pm.attempt);
+				Destroy(pm.door);
+				pm.active = false;
+			} 
+		}
+	}
+}
---
+++ Assets/Scripts/puzzle_script copia/PuzzleMaster.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PuzzleMaster : MonoBehaviour {
+
+	// 0 = Red
+	// 1 = Green
+	// 2 = Blue
+	public int[] correctSequence = new int[]{0,1,2};
+
+	public int attempt = 0;
+	public bool active = true;
+	public GameObject door;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public int Size (){
+		return correctSequence.Length;
+	}
+}
