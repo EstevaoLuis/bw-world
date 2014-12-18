70b9361561f13012e8a0105ad62f4a89e161184a
fixgamedev <felixlucashazen@gmail.com>
2014-12-18 20:05:50 +0100

    Boss Arena

---
+++ Assets/Scripts/BOSSARENASCRIPTS/spider_spawn.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spider_spawn : MonoBehaviour {
+
+	public GameObject enemy;
+	private Animator MagicAppereance;
+	private GameObject king;
+	private EnemyController enemy_king;
+	private int number_of_Swarm = 1;
+	private EnemyController [] spider;
+	private GameObject [] swarm;
+	public GameObject exp;
+	private GameObject exp_cop;
+
+	float dist;
+	float actual_dist;
+
+	void create_army(){
+
+		swarm = new GameObject[number_of_Swarm];
+		spider = new EnemyController[number_of_Swarm];
+		int i = 0;
+		for (i = 0; i< number_of_Swarm; i++) {
+			swarm [i] = (GameObject) Instantiate (enemy,new Vector2 (this.transform.position.x + i,this.transform.position.y + 25) ,transform.rotation); 
+			//Instantiate(exp, swarm[i].transform.position,transform.rotation);
+			//exp.animation.Stop();
+			//MagicAppereance.Play("AppearSpider");
+			spider [i] = swarm[i].GetComponent <EnemyController> ();
+		}
+		//Destroy (exp);
+		
+	}
+	bool swarm_creator(){
+
+		int i = 0;
+		for (i = 0; i < number_of_Swarm; i++) {
+			if(spider[i].getHealth() > 0){
+				return false;
+			}
+		}
+		create_army ();
+		return true;
+	}
+	bool check_The_king(){
+
+		if (enemy_king.getHealth () <= 0) {
+			int i = 0;
+			for(i = 0; i< number_of_Swarm; i ++){
+				if(swarm[i] != null){
+					//MagicAppereance.Play("AppearSpider");
+					Destroy(swarm[i]);
+				}
+			}
+			return false;
+		}
+		return true;
+
+	}
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		//king = (GameObject) Instantiate (enemy,new Vector2 (this.transform.position.x + 5*i,this.transform.position.y + 10) ,transform.rotation);
+		//MagicAppereance = gameObject.GetComponent <Animator> () as Animator;
+		king = GameObject.Find("Boss");
+		enemy_king = king.GetComponent<EnemyController> ();
+		create_army ();
+
+		int i = 0;
+
+		exp.animation.wrapMode = WrapMode.Once;
+
+		for (i = 0; i< number_of_Swarm; i++) {
+		
+			exp_cop = (GameObject)Instantiate(exp, swarm[i].transform.position,transform.rotation);
+		
+		}
+
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+		if (check_The_king ()) {
+			swarm_creator ();
+		}
+
+	}
+}
--- Assets/Scripts/Core scripts/PlayerController.cs
+++ Assets/Scripts/Core scripts/PlayerController.cs
@@ -20,6 +20,21 @@ public class PlayerController : MonoBehaviour {
 	void Start () {
 		animator = GetComponent<Animator> () as Animator;
 		direction = new Vector2(0.0f,-1.0f);
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+
+		GameObject virtualPad = GameObject.FindGameObjectWithTag ("joystick");
+		//joystick = virtualPad.GetComponent("Joystick") as Joystick;
+
+		//GameObject virtualPad = GameObject.FindGameObjectWithTag ("joystick");
+		//joystick = virtualPad.GetComponent("Joystick") as Joystick;
+		//GameObject[] joysticks = GameObject.FindGameObjectsWithTag ("joystick");
+		//joystick = joysticks [0].GetComponent("Joystick") as Joystick;
+
+=======
+>>>>>>> FETCH_HEAD
+>>>>>>> Stashed changes
 	}
 
 
@@ -27,6 +42,95 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+//		if (useJoystick) {
+//			if (joystick.position.y!=0 && Mathf.Abs(joystick.position.y)>Mathf.Abs(joystick.position.x)) {
+//				if(joystick.position.y < 0) {
+//					animator.Play ("WalkDown");
+//					direction = new Vector2 (0.0f, -1.0f);
+//					rigidbody2D.velocity = direction * speed * Mathf.Abs(joystick.position.y);
+//				}
+//				else {
+//					animator.Play ("WalkUp");
+//					direction = new Vector2 (0.0f, 1.0f);
+//					rigidbody2D.velocity = direction * speed * Mathf.Abs(joystick.position.y);
+//				}
+//			}
+//			else if(joystick.position.x!=0) {
+//				if(joystick.position.x < 0) {
+//					animator.Play ("WalkLeft");
+//					direction = new Vector2 (-1.0f, 0.0f);
+//					rigidbody2D.velocity = direction * speed * Mathf.Abs(joystick.position.x);
+//				}
+//				else {
+//					animator.Play ("WalkRight");
+//					direction = new Vector2 (1.0f, 0.0f);
+//					rigidbody2D.velocity = direction * speed * Mathf.Abs(joystick.position.x);
+//				}
+//			}
+//			else {
+//				rigidbody2D.velocity = new Vector2 (0, 0);
+//			}
+//				
+//		} else {
+
+
+//				if (useJoystick) {
+//						if (leftJoystick.position.y != 0 && Mathf.Abs (leftJoystick.position.y) > Mathf.Abs (leftJoystick.position.x)) {
+//								if (leftJoystick.position.y < 0) {
+//										animator.Play ("WalkDown");
+//										direction = new Vector2 (0.0f, -1.0f);
+//										rigidbody2D.velocity = direction * speed * Mathf.Abs (leftJoystick.position.y);
+//								} else {
+//										animator.Play ("WalkUp");
+//										direction = new Vector2 (0.0f, 1.0f);
+//										rigidbody2D.velocity = direction * speed * Mathf.Abs (leftJoystick.position.y);
+//								}
+//						} else if (leftJoystick.position.x != 0) {
+//								if (leftJoystick.position.x < 0) {
+//										animator.Play ("WalkLeft");
+//										direction = new Vector2 (-1.0f, 0.0f);
+//										rigidbody2D.velocity = direction * speed * Mathf.Abs (leftJoystick.position.x);
+//								} else {
+//										animator.Play ("WalkRight");
+//										direction = new Vector2 (1.0f, 0.0f);
+//										rigidbody2D.velocity = direction * speed * Mathf.Abs (leftJoystick.position.x);
+//								}
+//						} else {
+//								rigidbody2D.velocity = new Vector2 (0, 0);
+//						}
+//				
+//				} else {
+
+						if (Input.GetKey (KeyCode.DownArrow)) {
+								animator.Play ("WalkDown");
+								direction = new Vector2 (0.0f, -1.0f);
+								rigidbody2D.velocity = direction * speed;
+						} else if (Input.GetKey (KeyCode.UpArrow)) {
+								//transform.position += transform.up * MoveSpeed * Time.deltaTime;
+								animator.Play ("WalkUp");
+								direction = new Vector2 (0.0f, 1.0f);
+								rigidbody2D.velocity = direction * speed;
+						} else if (Input.GetKey (KeyCode.LeftArrow)) {
+								//transform.position -= transform.right * MoveSpeed * Time.deltaTime;
+								animator.Play ("WalkLeft");
+								direction = new Vector2 (-1.0f, 0.0f);
+								rigidbody2D.velocity = direction * speed;
+						} else if (Input.GetKey (KeyCode.RightArrow)) {
+								//transform.position += transform.right * MoveSpeed * Time.deltaTime;
+								animator.Play ("WalkRight");
+								direction = new Vector2 (1.0f, 0.0f);
+								rigidbody2D.velocity = direction * speed;
+						} else {
+								rigidbody2D.velocity = new Vector2 (0, 0);
+						}
+
+						if (Time.time > lastSpell + 0.1f) {
+
+=======
+>>>>>>> Stashed changes
 		if (useJoystick) {
 
 				if (leftJoystick.position.y != 0 && Mathf.Abs (leftJoystick.position.y) > Mathf.Abs (leftJoystick.position.x)) {
@@ -54,6 +158,7 @@ public class PlayerController : MonoBehaviour {
 				}
 		
 		} else {
+<<<<<<< Updated upstream
 
 				if (Input.GetKey (KeyCode.DownArrow)) {
 						animator.Play ("WalkDown");
@@ -81,6 +186,91 @@ public class PlayerController : MonoBehaviour {
 
 		if(Time.time > lastSpell + 0.1f) {
 
+=======
+
+				if (Input.GetKey (KeyCode.DownArrow)) {
+						animator.Play ("WalkDown");
+						direction = new Vector2 (0.0f, -1.0f);
+						rigidbody2D.velocity = direction * speed;
+				} else if (Input.GetKey (KeyCode.UpArrow)) {
+						//transform.position += transform.up * MoveSpeed * Time.deltaTime;
+						animator.Play ("WalkUp");
+						direction = new Vector2 (0.0f, 1.0f);
+						rigidbody2D.velocity = direction * speed;
+				} else if (Input.GetKey (KeyCode.LeftArrow)) {
+						//transform.position -= transform.right * MoveSpeed * Time.deltaTime;
+						animator.Play ("WalkLeft");
+						direction = new Vector2 (-1.0f, 0.0f);
+						rigidbody2D.velocity = direction * speed;
+				} else if (Input.GetKey (KeyCode.RightArrow)) {
+						//transform.position += transform.right * MoveSpeed * Time.deltaTime;
+						animator.Play ("WalkRight");
+						direction = new Vector2 (1.0f, 0.0f);
+						rigidbody2D.velocity = direction * speed;
+				} else {
+						rigidbody2D.velocity = new Vector2 (0, 0);
+				}
+		}
+>>>>>>> FETCH_HEAD
+
+		if(Time.time > lastSpell + 0.1f) {
+
+<<<<<<< HEAD
+								if (Input.GetKey (KeyCode.W)) {
+										GameInstance.instance.playerCastSpell ("Red 1", transform, direction);
+										//used.setType(1);
+										lastSpell = Time.time;
+								} else if (Input.GetKey (KeyCode.A)) {
+										GameInstance.instance.playerCastSpell ("Blue 1", transform, direction);
+										//used.setType(3);
+										lastSpell = Time.time;
+								} else if (Input.GetKey (KeyCode.D)) {
+										GameInstance.instance.playerCastSpell ("Green 1", transform, direction);
+										//used.setType(2);
+										lastSpell = Time.time;
+
+										if (useJoystick) {
+												if (Mathf.Abs (rightJoystick.position.x) > Mathf.Abs (rightJoystick.position.y)) {
+														if (rightJoystick.position.x < -0.5f) {
+																GameInstance.instance.playerCastSpell ("Red 1", transform, direction);
+																lastSpell = Time.time;
+														} else if (rightJoystick.position.x > 0.5f) {
+																GameInstance.instance.playerCastSpell ("Blue 1", transform, direction);
+																lastSpell = Time.time;
+														}
+												} else {
+														if (rightJoystick.position.y < -0.5f) {
+																GameInstance.instance.playerCastSpell ("Green 1", transform, direction);
+																lastSpell = Time.time;
+														} else if (rightJoystick.position.y > 0.5f) {
+																GameInstance.instance.playerCastSpell ("Red 4", transform, direction);
+																lastSpell = Time.time;
+														}
+												}
+
+										} else {
+												if (Input.GetKey (KeyCode.W)) {
+														GameInstance.instance.playerCastSpell ("Red 1", transform, direction);
+														//used.setType(1);
+														lastSpell = Time.time;
+												} else if (Input.GetKey (KeyCode.A)) {
+														GameInstance.instance.playerCastSpell ("Blue 1", transform, direction);
+														//used.setType(3);
+														lastSpell = Time.time;
+												} else if (Input.GetKey (KeyCode.D)) {
+														GameInstance.instance.playerCastSpell ("Green 1", transform, direction);
+														//used.setType(2);
+														lastSpell = Time.time;
+												} else if (Input.GetKey (KeyCode.S)) {
+														GameInstance.instance.playerCastSpell ("Red 4", transform, direction);
+														//used.setType(1);
+														lastSpell = Time.time;
+												}
+										}
+								}
+						}
+=======
+>>>>>>> Stashed changes
 			if (useJoystick){
 				if(Mathf.Abs (rightJoystick.position.x) > Mathf.Abs (rightJoystick.position.y)) {
 					if(rightJoystick.position.x < -0.5f) {
@@ -124,10 +314,9 @@ public class PlayerController : MonoBehaviour {
 					GameInstance.instance.playerCastSpell("Red 4",transform,direction);
 					//used.setType(1);
 					lastSpell = Time.time;
+>>>>>>> FETCH_HEAD
 				}
-			}
-		}
-	}
+
 
 
 	void OnCollisionEnter2D (Collision2D other) {
