ddcb8d51fc90203edc9b6d0eeafdab3b34a90ce7
Mastercava <mastercava@hotmail.it>
2014-12-18 13:59:33 +0100

    Mobile joystick

--- Assets/Scripts/Core scripts/PlayerController.cs
+++ Assets/Scripts/Core scripts/PlayerController.cs
@@ -14,14 +14,16 @@ public class PlayerController : MonoBehaviour {
 	private Spell used;
 	private GameObject spell;
 
-	private Joystick joystick;
+	public Joystick leftJoystick, rightJoystick;
 
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator> () as Animator;
 		direction = new Vector2(0.0f,-1.0f);
-		GameObject virtualPad = GameObject.FindGameObjectWithTag ("joystick");
-		joystick = virtualPad.GetComponent("Joystick") as Joystick;
+		//GameObject virtualPad = GameObject.FindGameObjectWithTag ("joystick");
+		//joystick = virtualPad.GetComponent("Joystick") as Joystick;
+		//GameObject[] joysticks = GameObject.FindGameObjectsWithTag ("joystick");
+		//joystick = joysticks [0].GetComponent("Joystick") as Joystick;
 	}
 
 
@@ -29,28 +31,28 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (useJoystick) {
-			if (joystick.position.y!=0 && Mathf.Abs(joystick.position.y)>Mathf.Abs(joystick.position.x)) {
-				if(joystick.position.y < 0) {
+			if (leftJoystick.position.y!=0 && Mathf.Abs(leftJoystick.position.y)>Mathf.Abs(leftJoystick.position.x)) {
+				if(leftJoystick.position.y < 0) {
 					animator.Play ("WalkDown");
 					direction = new Vector2 (0.0f, -1.0f);
-					rigidbody2D.velocity = direction * speed * Mathf.Abs(joystick.position.y);
+					rigidbody2D.velocity = direction * speed * Mathf.Abs(leftJoystick.position.y);
 				}
 				else {
 					animator.Play ("WalkUp");
 					direction = new Vector2 (0.0f, 1.0f);
-					rigidbody2D.velocity = direction * speed * Mathf.Abs(joystick.position.y);
+					rigidbody2D.velocity = direction * speed * Mathf.Abs(leftJoystick.position.y);
 				}
 			}
-			else if(joystick.position.x!=0) {
-				if(joystick.position.x < 0) {
+			else if(leftJoystick.position.x!=0) {
+				if(leftJoystick.position.x < 0) {
 					animator.Play ("WalkLeft");
 					direction = new Vector2 (-1.0f, 0.0f);
-					rigidbody2D.velocity = direction * speed * Mathf.Abs(joystick.position.x);
+					rigidbody2D.velocity = direction * speed * Mathf.Abs(leftJoystick.position.x);
 				}
 				else {
 					animator.Play ("WalkRight");
 					direction = new Vector2 (1.0f, 0.0f);
-					rigidbody2D.velocity = direction * speed * Mathf.Abs(joystick.position.x);
+					rigidbody2D.velocity = direction * speed * Mathf.Abs(leftJoystick.position.x);
 				}
 			}
 			else {
@@ -85,9 +87,25 @@ public class PlayerController : MonoBehaviour {
 		
 		if(Time.time > lastSpell + 0.1f) {
 			if (useJoystick){
-				if(joystick.tapCount >1) {
-					GameInstance.instance.playerCastSpell("Green 1",transform,direction);
-					lastSpell = Time.time;
+				if(Mathf.Abs (rightJoystick.position.x) > Mathf.Abs (rightJoystick.position.y)) {
+					if(rightJoystick.position.x < -0.5f) {
+						GameInstance.instance.playerCastSpell("Red 1",transform,direction);
+						lastSpell = Time.time;
+					}
+					else if(rightJoystick.position.x > 0.5f) {
+						GameInstance.instance.playerCastSpell("Blue 1",transform,direction);
+						lastSpell = Time.time;
+					}
+				}
+				else {
+					if(rightJoystick.position.y < -0.5f) {
+						GameInstance.instance.playerCastSpell("Green 1",transform,direction);
+						lastSpell = Time.time;
+					}
+					else if(rightJoystick.position.y > 0.5f) {
+						GameInstance.instance.playerCastSpell("Red 4",transform,direction);
+						lastSpell = Time.time;
+					}
 				}
 			}
 			else {
