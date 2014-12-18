247df1a888b1bca1d1ccfd2b266bf46c4e9eb673
Mastercava <mastercava@hotmail.it>
2014-12-19 00:11:26 +0100

    Other

--- Assets/Scripts/Events/Level 1/RunAway/SpiderGenerator.cs
+++ Assets/Scripts/Events/Level 1/RunAway/SpiderGenerator.cs
@@ -5,7 +5,7 @@ public class SpiderGenerator : MonoBehaviour {
 
 	private int spidersGenerated = 0;
 	private float lastGeneration = 0f;
-
+	private bool isActive = false;
 	private GameObject spider;
 
 	// Use this for initialization
@@ -15,21 +15,32 @@ public class SpiderGenerator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(spidersGenerated<5 && Time.time > lastGeneration + 5f) {
-			instantiateSpider();
-			spidersGenerated++;
-			lastGeneration = Time.time;
-		}
-		else if(spidersGenerated>=5 && spidersGenerated<20 && Time.time > lastGeneration + 2f) {
-			instantiateSpider();
-			spidersGenerated++;
-			lastGeneration = Time.time;
+		if (isActive) {
+				if (spidersGenerated < 5 && (lastGeneration == 0f || Time.time > lastGeneration + 5f)) {
+						instantiateSpider ();
+						spidersGenerated++;
+						lastGeneration = Time.time;
+				} else if (spidersGenerated >= 5 && spidersGenerated < 20 && Time.time > lastGeneration + 1f) {
+						instantiateSpider ();
+						spidersGenerated++;
+						lastGeneration = Time.time;
+				}
 		}
 	}
 
 	void instantiateSpider() {
 		GameObject newSpider = (GameObject) Instantiate(spider, transform.position, Quaternion.Euler(new Vector3(0,0,0)));
-		(newSpider.GetComponent ("EnemyController") as EnemyController).name = "Spider (Noob)";
-		Debug.Log ("Spider!");
+		int randType = Random.Range (0, 2);
+		if(randType > 0) (newSpider.GetComponent ("EnemyController") as EnemyController).enemyName = "Spider (Noob)";
+		else (newSpider.GetComponent ("EnemyController") as EnemyController).enemyName = "Spider";
+		//GameInstance.instance.playAnimation ();
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log (other.gameObject.tag);
+		if(other.gameObject.tag == "Player") {
+			isActive = true;
+			Debug.Log ("Yeah");
+		}
 	}
 }
