0dbe077c8d783a8b0e58a08d7bbf987ce17bd630
fixgamedev <felixlucashazen@gmail.com>
2014-12-29 22:20:29 +0100

    InvertedSnake (buggy unfinished)
    You are the food. Snake tries to catch yu!!!
    Right now trying to set the movement of the snake as it was a “chain”.
    the area is a 3x3 sphere matrix where the player will try to avoid the
    snake at all cost.

---
+++ Assets/Scripts/Events/SnakeEvent/matrixCreator.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class matrixCreator : MonoBehaviour {
+
+	// Use this for initialization
+	private int number_cells = 8;
+	//private int actual_cells = 0;
+	public GameObject cellTrap;
+	//private GameObject [] trapMatrix;
+
+
+	void Start () {
+	
+		create_trapMatrix ();
+
+	}
+
+	void create_trapMatrix(){
+		int i = 0;
+		int j = 0;
+		int k = 0;
+		int row_counter = 0;
+
+		while(row_counter < 9){
+
+			if (row_counter < 3) {
+				Instantiate (cellTrap, new Vector3 (transform.position.x, transform.position.y + 6.5f * i), transform.rotation);
+				i++;
+				row_counter++;
+			}
+			if (row_counter >= 3 && row_counter < 6) {
+				Instantiate (cellTrap, new Vector3 (transform.position.x + 6.5f, transform.position.y + 6.5f * j), transform.rotation);
+				j++;
+				row_counter++;
+			}
+			if (row_counter >= 6 && row_counter <= 8) {
+				Instantiate (cellTrap, new Vector3 (transform.position.x + 13f, transform.position.y + 6.5f * k), transform.rotation);
+				k++;
+				row_counter++;
+			}
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
