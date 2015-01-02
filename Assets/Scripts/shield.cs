791961fc00587f894f2e389cfa70c00274a48b2b
fixgamedev <felixlucashazen@gmail.com>
2015-01-02 19:06:39 +0100

    More Prototyping. Shield
    Trying to implement a shield that rotates around the player. Like a
    powerup.

---
+++ Assets/Scripts/shield.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shield : MonoBehaviour {
+
+	private GameObject Player;
+	public GameObject anim;
+	private GameObject shld;
+	// Use this for initialization
+
+	void Start () {
+
+		Player = GameObject.FindGameObjectWithTag ("Player");
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		int i = 0;
+		if (other.gameObject == Player) {
+			shld = (GameObject) Instantiate(anim,new Vector3(Player.transform.position.x,Player.transform.position.y + 5),transform.rotation);
+			Destroy(gameObject);
+		}
+
+	}
+
+//	void movement_of_shield(){
+//		if (anim != null) {
+//			anim.rigidbody2D.velocity = Vector3.up*5;
+//		}
+//
+//	}
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+		//movement_of_shield ();
+
+	}
+}
---
+++ Assets/Scripts/shieldSpell.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shieldSpell : MonoBehaviour {
+
+	private GameObject player;
+	private int quad;
+	private int radio = 5;
+	private int ang_velocity=2;
+	// Use this for initialization
+	void Start () {
+	
+		player = GameObject.FindGameObjectWithTag("Player");
+
+	}
+	int quadrant(){
+
+		if (player.transform.position.x < transform.position.x && player.transform.position.y < transform.position.y) {
+			return 1;
+		}
+		if (player.transform.position.x < transform.position.x && player.transform.position.y > transform.position.y) {
+			return -1;
+		}
+		if (player.transform.position.x > transform.position.x && player.transform.position.y < transform.position.y) {
+			return 2;
+		}
+		if (player.transform.position.x > transform.position.x && player.transform.position.y > transform.position.y) {
+			return -2;
+		}
+		return 0;
+
+	}
+	void moveRotate2D(){
+
+		if (quadrant() == 1) {
+			transform.position = new Vector3(player.transform.position.x+radio*ang_velocity,player.transform.position.y+radio*ang_velocity);	
+		}
+		if (quadrant() == -1) {
+			transform.position = new Vector3(player.transform.position.x+radio*ang_velocity,player.transform.position.y-radio*ang_velocity);	
+		}
+		if (quadrant() == 2) {
+			transform.position = new Vector3(player.transform.position.x-radio*ang_velocity,player.transform.position.y+radio*ang_velocity);	
+		}
+		if (quadrant() == -2) {
+			transform.position = new Vector3(player.transform.position.x-radio*ang_velocity,player.transform.position.y-radio*ang_velocity);	
+		}
+
+	}
+	// Update is called once per frame
+	void Update () {
+		quadrant ();
+		moveRotate2D ();
+		//transform.RotateAround (Player.transform.position, Vector2.up, 20 * Time.deltaTime);
+		//transform.Rotate (0, 1, 0);
+		//transform.rigidbody2D.angularVelocity = 20;
+
+
+	}
+}
