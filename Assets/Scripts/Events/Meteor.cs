6ff7178c22df64264edbee93870681289fa3dd25
Mastercava <mastercava@hotmail.it>
2015-01-08 13:57:52 +0100

    Meteors and other stuff B)

---
+++ Assets/Scripts/Events/Meteor.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Meteor : MonoBehaviour {
+
+	public float hitTime = 0.3f;
+	private float explosionFinishedTime = 1f; 
+	public int meteorDamage = 30;
+
+	private float startTime;
+	private bool explosionActivated = false;
+	private CircleCollider2D collider;
+
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time;
+		collider = GetComponent ("CircleCollider2D") as CircleCollider2D;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > startTime + hitTime ) {
+			if(!explosionActivated) {
+				explosionActivated = true;
+				audio.Play();
+			}
+			collider.radius = Mathf.Lerp(0f,10f,Time.time-startTime);
+			//collider.size = new Vector3(Mathf.Lerp(0f,8f,Time.time-startTime),Mathf.Lerp(0f,5f,Time.time-startTime),4f);
+			if(Time.time > startTime + explosionFinishedTime) Destroy (this.gameObject);
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D other) {
+		if (other.gameObject.tag == "Player") {
+			GameInstance.instance.damagePlayer(meteorDamage);
+		}
+	}
+}
---
+++ Assets/Scripts/Events/MeteorArea.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeteorArea : MonoBehaviour {
+
+	public float areaSize;
+	public float generationPeriod = 5f;
+	public float waitBeforeInstantiating = 3f;
+
+	private float lastGeneration = 0f;
+	private GameObject meteorPrefab;
+	private Vector3 newPosition;
+
+	// Use this for initialization
+	void Start () {
+		meteorPrefab = (GameObject) Resources.Load ("Events/Meteor");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > lastGeneration + generationPeriod) {
+			newPosition = new Vector3(transform.position.x + Random.Range(-areaSize,areaSize)+4f,transform.position.y + Random.Range(-areaSize,areaSize)+6f,-2f);
+			GameInstance.instance.playAnimation("Target",new Vector3(newPosition.x-4f,newPosition.y-6.5f,-1f));
+			Invoke("instantiateMeteor",waitBeforeInstantiating);
+			lastGeneration = Time.time;
+		}
+	}
+
+	private void instantiateMeteor() {
+		Instantiate(meteorPrefab, newPosition, new Quaternion(0,0,0,1));
+	}
+}
