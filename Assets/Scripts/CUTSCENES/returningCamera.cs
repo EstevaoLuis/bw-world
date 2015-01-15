f79d03fe5447b14e401ff08dc333a7742ef42b38
fixgamedev <felixlucashazen@gmail.com>
2015-01-15 23:15:57 +0100

    Fade In and out camera :)

---
+++ Assets/Scripts/CUTSCENES/returningCamera.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class returningCamera : MonoBehaviour {
+
+	//public GameObject fadeInOut;
+//	public GameObject r = null;
+	public GameObject Cam = null;
+	private int trigger = 0;
+	private int trigger_2 = 0;
+	public Vector3 desirePosition;
+	private GameObject player;
+	private PlayerController pl;
+	private Camera cameratofade;
+	private float timer;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		player = GameObject.FindGameObjectWithTag("Player");
+		pl = player.GetComponent <PlayerController> ();
+
+		cameratofade = Cam.GetComponent<Camera> ();
+
+
+	}
+
+
+	void OnCollisionEnter2D (Collision2D other){
+
+		if (other.gameObject.tag == "Player") {
+			Invoke ("off",3);
+			this.collider2D.enabled = false;
+		}
+
+	}
+
+//	void fade_in(){
+//		if (trigger == 2) {
+//			Invoke ("lights_on",3);
+//			trigger_2 = 1;
+//		}
+//	}
+
+	void fin(){
+
+	}
+
+	void lights_on(){
+
+		cameratofade.enabled = true;
+
+	}
+	void off(){
+
+		cameratofade.enabled = false;
+		pl.isAvailable (false);
+		GameInstance.instance.movePlayer (desirePosition);
+		GameInstance.instance.moveCamera (desirePosition);
+		Invoke ("on",3);
+
+	}
+	void on(){
+		
+		cameratofade.enabled = true;
+		pl.isAvailable (true);
+		//cameratofade.enabled = true;
+		
+	}
+	
+
+}
