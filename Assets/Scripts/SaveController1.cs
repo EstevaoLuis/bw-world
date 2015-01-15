f88db247f3ba18ae19add8d198da4db52dac006f
Estevao Luis <estevao.costamoura@dev4side.com>
2015-01-15 11:41:35 +0100

    Save Slot Menu :-)

---
+++ Assets/Scripts/SaveController1.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SaveController1 : MonoBehaviour {
+
+	public GameObject pauseMenu;
+	public GameObject saveMenu;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.K)) {
+			Resume();
+		}
+	}
+
+	public void Save(int slot){
+		Debug.Log ("Slot-> " + slot);
+		// TODO Real implementation
+		Resume ();
+	}
+
+	public void Resume(){
+		saveMenu.SetActive (false);
+		pauseMenu.SetActive (true);
+	}
+}
