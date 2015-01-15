8ce2d2e615bb3b97be0bb30dbff7d3bc7f4c4837
fixgamedev <felixlucashazen@gmail.com>
2015-01-15 20:39:04 +0100

    BlueWorldboss ( difficult )

--- Assets/Scripts/BOSSARENASCRIPTS/IceSwordsRandom.cs
+++ Assets/Scripts/BOSSARENASCRIPTS/IceSwordsRandom.cs
@@ -7,39 +7,50 @@ public class IceSwordsRandom : MonoBehaviour {
 	//public GameObject iceBomb;
 	public GameObject target;
 	private GameObject aux;
-	private float timer = 2f;
+	private float trigger = 0;
+	private float timer = 2;
 	private float timer_2 = 7f;
 	private float speed = 4f;
 	//private float timer_2 = 2f;
 	private int nam;
 	private int pos = 1;
-	private int trigger = 0;
 	// Use this for initialization
 	void Start () {
 		target = GameObject.FindGameObjectWithTag ("Player");
 	}
-
-
 	void random_generator_ice_sword(){
 
 		Vector3 random_pos = new Vector3 (Random.Range (-5 + target.transform.position.x, target.transform.position.x + 5)
 		                                  , Random.Range (-5 + target.transform.position.y, target.transform.position.y + 5));
 
 
-		
-		if (Time.time > timer) {
-			aux = (GameObject)Instantiate (IceSwords,random_pos, transform.rotation);
-			aux.rigidbody2D.velocity = (Vector3.left + Vector3.down) * speed;
-			timer = Time.time + 2f;
+		aux = (GameObject)Instantiate (IceSwords,random_pos, transform.rotation);
+		aux.rigidbody2D.velocity = (Vector3.left + Vector3.down) * speed;
+
 
 		}
+	
 
-	}
+//	void OnCollisionEnter2D (Collision2D other){
+//		
+//		if (other.gameObject.tag == "Player") {
+//			trigger = 1;		
+//		}
+//	}
+
 	void OnTriggerEnter2D (Collider2D other){
+
 		if (other.gameObject.tag == "Player") {
 			trigger = 1;
 		}
+
 	}
+
+	void start_shooting(){
+		Invoke("random_generator_ice_sword",timer);
+		timer = timer + 1;
+	}
+
 //	void onSight(){
 //		float dis = Vector3.Distance(transform.position,target.transform.position);
 //		if( dis < 5){
@@ -51,8 +62,8 @@ public class IceSwordsRandom : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if (trigger == 1) {
-			random_generator_ice_sword ();
+	if (trigger == 1) {
+			start_shooting();
 		}
 
 	}
