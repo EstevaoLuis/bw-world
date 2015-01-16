299230db781cabbe9eb9c23f4513e7072c5fe69f
fixgamedev <felixlucashazen@gmail.com>
2015-01-16 01:38:33 +0100

    AvalancheFolder

---
+++ Assets/Scripts/GemSpellEvent.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GemSpellEvent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+//	void OnCollisionEnter2D (Collision2D other){
+//
+//		GameInstance.instance.set
+//	
+//		
+//	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
