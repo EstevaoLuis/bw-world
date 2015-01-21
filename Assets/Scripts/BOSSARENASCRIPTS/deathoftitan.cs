023e65cc339e4d7e62802edeed081afb90830750
Mastercava <mastercava@hotmail.it>
2015-01-21 12:16:42 +0100

    Green world global events

--- Assets/Scripts/BOSSARENASCRIPTS/deathoftitan.cs
+++ Assets/Scripts/BOSSARENASCRIPTS/deathoftitan.cs
@@ -3,29 +3,31 @@ using System.Collections;
 
 public class deathoftitan : MonoBehaviour {
 
-	private EnemyController tit;
-	public GameObject door;
+	private PlayerController controller;
+	private EnemyController enemy;
+	private bool isActivated = false;
 
 	// Use this for initialization
 	void Start () {
 	
-		tit = this.GetComponent<EnemyController> ();
+		enemy = GetComponent<EnemyController> ();
+		controller = GameInstance.instance.getPlayerController ();
 
-
-	}
- 	bool titan_death(){
-
-		if (tit.getHealth () <= 0) {
-			Destroy(door);
-			return true;
-		}
-
-		return false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		titan_death ();
+		if (enemy.getHealth () <= 0 && !isActivated) {
+			isActivated = true;
+			GameInstance.instance.playAudio("Darkness8");
+			Invoke ("activateTeleport",3f);
+			Debug.Log ("Boss sconfitto");
+			GameInstance.instance.playAnimation("Wind Attack",new Vector3(transform.position.x,transform.position.y-4f,0f));
+			Object prefab2 = Resources.Load("Events/Teleport") as Object;
+			Instantiate(prefab2,new Vector3(transform.position.x,transform.position.y,0.1f), new Quaternion(0f,0f,0f,1f));
+			QuestManager.instance.setNewTarget(new Vector3(transform.position.x,transform.position.y,0.1f));
+		}
 
 	}
+	
 }
