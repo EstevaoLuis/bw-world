9d37f78e691c085544c6c843c2eae78ed6437116
Estevao Luis <estevao.costamoura@dev4side.com>
2015-01-21 12:23:25 +0100

    ExplosionColorGem
    - to use for the gate to the blue world
    - hit with the specific color many times and it will explode

---
+++ Assets/Scripts/ExplosionColorGem.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplosionColorGem : MonoBehaviour {
+
+	private SpriteRenderer sprite;
+	public string color = "green";
+	public float intensity = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		sprite = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void kolorMe(){
+		//Color kolor = sprite.color;
+		float red = sprite.color.r;
+		float green = sprite.color.g;
+		float blue = sprite.color.b;
+		//float alpha = sprite.color.a;
+		bool destructionBool = false;
+
+
+		Debug.Log ("RGB: " + red + " " + green + " " + blue);
+
+		if (color != "red")	red = red - intensity;
+		if (color != "blue") blue = blue - intensity;
+		if (color != "green") green = green - intensity;
+
+		Debug.Log ("RGB: " + red + " " + green + " " + blue);
+
+		if (red <= 0f || green <= 0f || blue <= 0f) {
+			red = 0f;
+			blue = 0f;
+			destructionBool = true;
+		}
+
+		Debug.Log ("RGB: " + red + " " + green + " " + blue);
+
+		sprite.color = new Color (red, green, blue);
+
+		if (destructionBool)
+			Destroy (gameObject);
+			//gameObject.SetActive (false);
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		if (other.gameObject.tag == "Spell") {
+			Spell spellParameters = (Spell)other.gameObject.GetComponent ("Spell");
+			string kolor = spellParameters.color;
+			
+			if (kolor.Equals(color)){
+				kolorMe();
+			}
+		}
+	}
+}
