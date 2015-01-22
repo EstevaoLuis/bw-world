34cd3b294fe834d6153e0e449f7bcf1ad46f2c1d
fixgamedev <felixlucashazen@gmail.com>
2015-01-22 17:17:56 +0100

    blueWorldAvalanchefixed

--- Assets/Scripts/AVALANCHE/Ava_Move.cs
+++ Assets/Scripts/AVALANCHE/Ava_Move.cs
@@ -4,30 +4,47 @@ using System.Collections;
 public class Ava_Move : MonoBehaviour {
 
 	private GameObject target;
-	private float speed= 4f;
+	private GameObject Spell;
+	public float speed;
 	private float timer = 1f; 
 	float dist;
+	int avoidcatch;
 	// Use this for initialization
 	void Start () {
 	
 		target=GameObject.FindGameObjectWithTag("Player");
+
 	}
 	void OnCollisionEnter2D (Collision2D other){
-				if (other.gameObject.tag == "Player") {
-						//rigidbody2D.isKinematic = true;
-						GameInstance.instance.damagePlayer (7);
-				}
-		}
 
+//		if (Spell != null) {
+//
+//			Physics2D.IgnoreCollision (this.collider2D, Spell.collider2D);
+//		}
 
-
+		if (other.gameObject.tag == "Player") {
+						
+				GameInstance.instance.damagePlayer (10);
+						
+			}
+			transform.Translate( Vector3.forward*Time.deltaTime); 
+				
+	}
+	
+	
+	
 	
 	// Update is called once per frame
 	void Update () {
+
+//		Spell = GameObject.FindGameObjectWithTag("Spell");
+
 		dist = Vector3.Distance (target.transform.position, transform.position);
 		if(dist < 30){
 		transform.position = Vector3.MoveTowards (transform.position, target.transform.position, speed * Time.deltaTime);
-
+//		if(Vector3.Distance(transform.position,target.transform.position) < avoidcatch){
+//				transform.position = transform.position - new Vector3(4,4,0);
+//			}
 		}
 	//	print (1.0 / Time.deltaTime);
 	}
