af2ef41614a6d4e62a5b84d246dda94094178307
Estevao Luis <estevao.costamoura@dev4side.com>
2015-01-22 14:19:56 +0100

    EnemyHealthBar applied on Mantis and Titan prefabs :v:

--- Assets/Scripts/EnemyHealthBar.cs
+++ Assets/Scripts/EnemyHealthBar.cs
@@ -8,26 +8,22 @@ public class EnemyHealthBar : MonoBehaviour {
 	private EnemyController controller;
 	public GameObject enemy;
 	private Slider health;
-	//public Canvas canvas;
 	private Camera camera;
+	public float barPosition;
 
 	// Use this for initialization
 	void Start () {
 		controller = enemy.GetComponent<EnemyController> ();
 		health = GetComponent<Slider> ();
 
-		Debug.Log ("ALEX KIDD");
 		JSONNode parameters = GameInstance.instance.getEnemyParameters (controller.enemyName);
-		Debug.Log ("DAVID DUNCAN");
 
 		int life = parameters ["health"].AsInt;
 
 		health.maxValue = life;
-		//Debug.Log (controller.getHealth ());
 		health.minValue = 0;
 		health.value = life;
 
-		//camera = (Camera)GameObject.Find ("Camera");
 		camera = Camera.main;
 	}
 	
@@ -35,10 +31,10 @@ public class EnemyHealthBar : MonoBehaviour {
 	void Update () {
 		if (enemy == null || controller.getHealth() <= 0) {
 				Debug.Log ("killed!");	
-				Destroy (gameObject);
+				Destroy (health);
 		}
 
-		Vector3 newPosition = enemy.transform.position + 2.8f * Vector3.up;
+		Vector3 newPosition = enemy.transform.position + barPosition * Vector3.up;
 		health.transform.position = (camera.WorldToScreenPoint (newPosition));
 		health.value = controller.getHealth ();
 	}
