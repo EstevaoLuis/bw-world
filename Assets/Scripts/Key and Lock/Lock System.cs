7ea738246feec55fbe503246037c0ec4e76a8478
Estevao Luis <estevao.costamoura@dev4side.com>
2015-01-28 13:51:55 +0100

    Key and Lock Puzzle

---
+++ Assets/Scripts/Key and Lock/Lock System.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockSystem : MonoBehaviour {
+
+	public int totalKeys = 1;
+	private int keysFound = 0;
+
+	public GameObject door;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		if (other.gameObject.tag == "Player") {
+			if (totalKeys == keysFound){
+				Destroy (door);
+				Destroy (gameObject);
+			}
+		}
+	}
+
+	public void KeyFound(){
+		keysFound++;
+	}
+
+}
