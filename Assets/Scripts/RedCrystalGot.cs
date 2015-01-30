b4b9122bbddd32393c360a5c056b374576bf708d
Estevao Luis <estevao.costamoura@dev4side.com>
2015-01-30 11:24:38 +0100

    Red World boss battle events :ant: :fire:

---
+++ Assets/Scripts/RedCrystalGot.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RedCrystalGot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerExit2D (Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			//GameInstance.instance.movePlayer (new Vector3 (0, 0));
+			GameInstance.instance.moveGameSystem (new Vector3 (0, 0));
+		}
+	}
+
+}
---
+++ Assets/Scripts/TriggerActivator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerActivator : MonoBehaviour {
+
+	public GameObject[] objectsToActivate;
+	public GameObject[] objectsToDeactivate;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			foreach (GameObject objectToActivate in objectsToActivate)
+					objectToActivate.SetActive (true);
+			foreach (GameObject objectToDeactivate in objectsToDeactivate)
+					objectToDeactivate.SetActive (false);
+			gameObject.SetActive(false);
+		}
+	}
+
+}
